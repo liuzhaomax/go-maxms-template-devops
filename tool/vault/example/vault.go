f39e0bd328134fbe7f1499a2952347624079fc77
LIUZHAO <liuzhaomax@163.com>
2023-12-21 22:46:15 +0800

    vault

---
+++ tool/vault/example/vault.go
@@ -0,0 +1,86 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	vault "github.com/hashicorp/vault/api"
+)
+
+const (
+	username  = "liuzhao1"
+	password  = "liuzhao78"
+	vaultAddr = "http://127.0.0.1:8200"
+	TOKEN     = "hvs.MUT9lFPIUdTbafLaovT5bTK1"
+	TOKEN1    = "hvs.CAESIABDu07W6mYiMoID4VI1XFzTjUUkeEN8HFBMTgpEt0abGh4KHGh2cy5RWldEREpxRUdoeEtEdlg5V3JhNGh2TTI"
+)
+
+var httpClient = &http.Client{
+	Timeout: 10 * time.Second,
+}
+
+func userpassLogin() (string, error) {
+	// create a vault client
+	client, err := vault.NewClient(&vault.Config{Address: vaultAddr, HttpClient: httpClient})
+	if err != nil {
+		return "", err
+	}
+
+	// to pass the password
+	options := map[string]interface{}{
+		"password": password,
+	}
+	path := fmt.Sprintf("auth/userpass/login/%s", username)
+
+	// PUT call to get a token
+	secret, err := client.Logical().Write(path, options)
+	if err != nil {
+		return "", err
+	}
+
+	token := secret.Auth.ClientToken
+	return token, nil
+}
+
+func main() {
+	// Authenticate and obtain a token
+	token, err := userpassLogin()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Token:", token)
+
+	// Create a new Vault client with the obtained token
+	client, err := vault.NewClient(&vault.Config{Address: vaultAddr, HttpClient: httpClient})
+	if err != nil {
+		panic(err)
+	}
+
+	// Set the token for subsequent requests
+	client.SetToken(token)
+	client.SetNamespace("cn/corp/nonprod/depa/go-maxms-template-me/sit/secrets")
+	kvPath := "kv"
+	ctx := context.Background()
+
+	// Make a read request to Vault
+	secret, err := client.KVv2(kvPath).Get(ctx, client.Namespace())
+	if err != nil {
+		panic(err)
+	}
+	// Print the response for troubleshooting
+	fmt.Println("Vault Response:", secret)
+
+	b, err := json.Marshal(secret.Data)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Secret Data:", string(b))
+}
