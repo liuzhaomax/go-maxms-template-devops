ccc21552828569afafc0e65a6f80a489838a7460
LIUZHAO <liuzhaomax@163.com>
2024-01-30 11:47:18 +0800

    remotely deploy

--- tool/jenkins/config/harbor/deploy.sh
+++ tool/jenkins/config/harbor/deploy.sh
@@ -7,37 +7,38 @@ version=$4
 container_port=$5
 host_port=$6
 environment=$7
+deployment_server_ip=$8
 
 # 确保没有container在运行
-containerID=$(docker ps -a | grep "${project}" | awk '{print $1}')
+containerID=$(docker -H tcp://$deployment_server_ip:2375 ps -a | grep "${project}" | awk '{print $1}')
 
-echo "Container ID: $containerID"
+echo "History Container ID: $containerID"
 
 if [ "$containerID" != "" ]; then
-  docker stop "$containerID"
-  docker rm "$containerID"
+  docker -H tcp://$deployment_server_ip:2375 stop "$containerID"
+  docker -H tcp://$deployment_server_ip:2375 rm "$containerID"
 fi
 
-# 确保没有同名image
+# 清除同名image
+imageIDRemote=$(docker -H tcp://$deployment_server_ip:2375 images | grep "${project}" | awk '{print $3}' | head -n 1)
+
+echo "History Image ID Remote: $imageIDRemote"
+
+if [ "$imageIDRemote" != "" ]; then
+  docker -H tcp://$deployment_server_ip:2375 rmi "$imageIDRemote"
+fi
+
+# 即将部署的镜像
 imageName="$harbor_addr/$harbor_repo/$project:$version"
 
 echo "Image Name: $imageName"
 
-tag=$(docker images | grep "${project}" | awk '{print $2}')
+# 远程登录harbor
+docker -H tcp://$deployment_server_ip:2375 login -u admin -p Harbor12345 "$harbor_addr"
 
-echo "Image Tag: $tag"
+docker -H tcp://$deployment_server_ip:2375 pull "$imageName"
 
-# shellcheck disable=SC2039
-if [ "$tag" != "$version" ]; then
-  docker rmi "$imageName"
-fi
-
-# 登录harbor
-docker login -u admin -p Harbor12345 "$harbor_addr"
-
-docker pull "$imageName"
-
-docker run \
+docker -H tcp://$deployment_server_ip:2375 run \
   --name="$project" \
   -d \
   --restart=always \
@@ -49,3 +50,12 @@ docker run \
   "$imageName"
 
 echo "SUCCESS: Container Created"
+
+# 部署后，清除jenkins服务器产生的image
+imageIDLocal=$(docker images | grep "${project}" | awk '{print $3}' | head -n 1)
+
+echo "History Image ID Local: $imageIDLocal"
+
+if [ "$imageIDLocal" != "" ]; then
+  docker rmi -f "$imageIDLocal"
+fi
